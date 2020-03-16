85a9dd33dd27ca1f40f1a5eeb6c533f466ba16d4
Shiroi <fabian_frontczak@wp.pl>
2020-03-16 22:14:56 +0100

    Dodano szablon aplikacji

---
+++ Biometria/Biometria/Forms/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Biometria
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.loadedImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loadedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loadImageButton.Location = new System.Drawing.Point(12, 12);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(93, 40);
+            this.loadImageButton.TabIndex = 0;
+            this.loadImageButton.Text = "Load";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // loadedImage
+            // 
+            this.loadedImage.Location = new System.Drawing.Point(0, 58);
+            this.loadedImage.Name = "loadedImage";
+            this.loadedImage.Size = new System.Drawing.Size(92, 62);
+            this.loadedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.loadedImage.TabIndex = 1;
+            this.loadedImage.TabStop = false;
+            this.loadedImage.Click += new System.EventHandler(this.LoadedImage_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.loadedImage);
+            this.Controls.Add(this.loadImageButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "MainForm";
+            this.Text = "Biometryczne wspomaganie komunikacji człowiek-komputer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadedImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox loadedImage;
+    }
+}
+
---
+++ Biometria/Biometria/Forms/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using ImageSoundProcessing.Factories;
+using ImageSoundProcessing.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biometria
+{
+    public partial class MainForm : Form
+    {
+        private string _imagePath;
+        private Bitmap _originalBitmap;
+
+        public string ImagePath { get => _imagePath; set => _imagePath = value; }
+
+        public Bitmap OriginalBitmap { get => _originalBitmap; set => _originalBitmap = value; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            string imagePath = Path.GetImagePath();
+            if (!imagePath.Equals(""))
+            {
+                _imagePath = imagePath;
+                _originalBitmap = BitmapFactory.CreateBitmap(imagePath);
+                loadedImage.Image = _originalBitmap;
+            }
+        }
+
+        private void LoadedImage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
