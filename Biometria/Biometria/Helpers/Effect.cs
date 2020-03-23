8da4a17557d51c167f6624daf6f22365141f00d5
Mantis1262 <Mateuszkarolak3@o2.pl>
2020-03-23 11:04:31 +0100

    Dodanie operacji zamiany na odcienie szarości

---
+++ Biometria/Biometria/Helpers/Effect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biometria.Helpers
+{
+    class Effect
+    {
+        public static Bitmap GrayMode(Bitmap original)
+        {
+            Bitmap processedBmp = new Bitmap(original.Width, original.Height);
+            LockBitmap originalBitmapLock = new LockBitmap(original);
+            LockBitmap processedBitmapLock = new LockBitmap(processedBmp);
+            originalBitmapLock.LockBits(ImageLockMode.ReadOnly);
+            processedBitmapLock.LockBits(ImageLockMode.WriteOnly);
+
+            for (int i = 0; i < originalBitmapLock.Width; i++)
+            {
+                for (int j = 0; j < originalBitmapLock.Height; j++)
+                {
+                    Color temp = originalBitmapLock.GetPixel(i, j);
+                    int gray = (int)(0.299 * temp.R + 0.587 * temp.G + 0.114 * temp.B);
+                    processedBitmapLock.SetPixel(i, j, Color.FromArgb(temp.A, (byte)gray, (byte)gray, (byte)gray));
+                }
+            }
+
+            originalBitmapLock.UnlockBits();
+            processedBitmapLock.UnlockBits();
+            return processedBmp;
+        }
+    }
+}
